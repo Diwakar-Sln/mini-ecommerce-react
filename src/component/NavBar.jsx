058bebf853696bd29b5005eecebcba5c34c770cd
Diwakar1210 <diwakar.b1999@gmail.com>
2026-09-07 11:59:53 +0530

    Logout done

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -1,27 +1,38 @@
 import React, { useContext } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { AuthContext } from '../context/AuthContext'
 
 const NavBar = () => {
-   const {logout} = useContext(AuthContext);
-  return (
-    <div style={{padding:"20px"}}>
-        <ul style={{display:"flex", alignItems:"center", justifyContent:"space-between"}}>
-            <li>
-                <Link to="/home"> <h3>Logo</h3> </Link>
-            </li>
-            <li style={{display:"flex", alignItems:"center", gap:"20px"}}>
-                <Link to="/home">Home</Link>
-                <Link to="/cart">Cart</Link>
-            </li>
-            <li style={{display:"flex", alignItems:"center", gap:"20px"}}>
-                <Link to="/">Login</Link>
-                <Link to="/">SignUp</Link>
-                <Link onClick={()=>logout()} >Logout</Link>
-            </li>
-        </ul>
-    </div>
-  )
+    const { logout, user } = useContext(AuthContext);
+    const navigate = useNavigate();
+    const handleLogout = () => {
+        logout();
+        navigate("/");
+    }
+    return (
+        <div style={{ padding: "20px" }}>
+            <ul style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
+                <li>
+                    <Link to="/home"> <h3>Logo</h3> </Link>
+                </li>
+                <li style={{ display: "flex", alignItems: "center", gap: "20px" }}>
+                    <Link to="/home">Home</Link>
+                    <Link to="/cart">Cart</Link>
+                </li>
+                {
+                    !user ?
+                        <li style={{ display: "flex", alignItems: "center", gap: "20px" }}>
+                            <Link to="/">Login</Link>
+                            <Link to="/">SignUp</Link>
+                        </li>
+                        : <li style={{ display: "flex", alignItems: "center", gap: "20px" }}>
+                            <span>{user.email}</span>
+                            <Link onClick={handleLogout} >Logout</Link>
+                        </li>
+                }
+            </ul>
+        </div>
+    )
 }
 
 export default NavBar
--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,14 +1,11 @@
 import React, { useContext } from 'react'
 import NavBar from '../component/NavBar'
 import { Outlet } from 'react-router-dom'
-import { AuthContext } from '../context/AuthContext'
 
 const MainLayout = () => {
-    const {user} = useContext(AuthContext);
     return (
         <div>
             <NavBar />
-            <h1>{user?.email}</h1>
             <main style={{padding:"30px"}}>
                 <Outlet />
             </main>
